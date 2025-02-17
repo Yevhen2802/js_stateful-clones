82ff582a0e81b8b6da5b823d101497897619a2ab
Your_Name Your_Surname <your@email>
2025-02-17 23:04:50 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const stateHistory = [];
+
+  for (const types of actions) {
+    if (types.type === 'addProperties') {
+      currentState = { ...currentState, ...types.extraData };
+    }
+
+    if (types.type === 'clear') {
+      currentState = {};
+    }
+
+    if (types.type === 'removeProperties') {
+      currentState = { ...currentState };
+      types.keysToRemove.forEach((key) => delete currentState[key]);
+    }
+
+    stateHistory.push(currentState);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
